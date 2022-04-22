bc6db3036c99a8e364d298d683a969fe3e1b594d
harryNeo9 <harishankarpv9@gmail.com>
2022-04-22 10:55:20 +0530

    cors error fix

--- server/index.js
+++ server/index.js
@@ -11,7 +11,10 @@ const cors = require('cors')
 connectDB()
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: `http://localhost:3000`,  //react's address
+    credentials: true
+}));
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
