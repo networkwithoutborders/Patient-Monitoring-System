f441902dae8601cc5a30c295ad14768e73610655
harryNeo9 <harishankarpv9@gmail.com>
2022-04-22 11:14:10 +0530

    cors fix

--- server/index.js
+++ server/index.js
@@ -11,10 +11,34 @@ const cors = require('cors')
 connectDB()
 
 const app = express()
-app.use(cors({
-    origin: `http://localhost:3000`,  //react's address
-    credentials: true
-}));
+
+
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
+
+
+// app.use(cors({
+//     origin: `https://pms-mernapp-demo.herokuapp.com`,  //react's address
+//     credentials: true
+// }));
+
+// const corsOptions = {
+//     origin: ["https://pms-mernapp-demo.herokuapp.com"],
+//     preflightContinue:false,
+//     credentials: true
+//   }
+  
+//   app.use(cors(corsOptions));
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
