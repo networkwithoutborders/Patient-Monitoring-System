ab73381b745149c7caff0415da6435f62d1103d0
Lucifer8729 <72292326+Lucifer8729@users.noreply.github.com>
2022-04-22 11:17:02 +0530

    switched patient register to heroku

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8000" });
+const API = axios.create({ baseURL: "https://pms-mernapp-demo.herokuapp.com" });
 
 // API calls to REGISTER_PATIENT
 export const registerPatient = (form) =>
