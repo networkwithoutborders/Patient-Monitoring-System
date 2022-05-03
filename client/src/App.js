72012630e855a9ad38b9a2e2af471e53935a6a71
Lucifer8729 <72292326+Lucifer8729@users.noreply.github.com>
2022-05-03 18:20:34 +0530

    added qr code page after pre registration

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,22 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Registration from "./Pages/Registration";
-import Patient from "./Pages/Patient";
 import HomeScreen from "./Pages/HomeScreen";
 import SignIn from "./Pages/SignIn";
 import PreRegistration from "./Pages/PreRegistration";
+import PatientQrCode from "./Pages/PatientQrCode";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/registration" element={<Registration />} />
-			<Route path="/patient/:id" element={<Patient />} />
 			<Route path="/" element={<HomeScreen />} />
 			<Route path="/signIn" element={<SignIn />} />
 			<Route
 				path="/paramedic/pre-registration"
 				element={<PreRegistration />}
 			/>
+			<Route path="/patient/QR/:id" element={<PatientQrCode />} />
 		</Routes>
 	);
 }
