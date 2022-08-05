bf7cc9a8321351e6d5bee783b4e5ddb37b2ccf70
S Ajay <ajay.satishpriya@gmail.com>
2022-08-05 19:24:49 +0530

    💄  PDCopd ui-global

--- client/src/components/PatientDetailCard/PatientDetailCardOpd.jsx
+++ client/src/components/PatientDetailCard/PatientDetailCardOpd.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 import {
   Card,
   CardActionArea,
   CardContent,
   Grid,
   Typography,
-} from "@mui/material";
-import { useNavigate } from "react-router-dom";
-
+} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import Styles from './PatientDetailCardOpd.module.css';
 const PatientDetailCard = ({
   orderNo,
   name,
@@ -23,7 +23,7 @@ const PatientDetailCard = ({
     navigate(`/doctor/opd/${uhid}`);
   };
   return (
-    <Card elevation={4} sx={{ width: "100%", borderRadius: "20px" }}>
+    <Card elevation={4} sx={{ width: '100%', borderRadius: '20px' }}>
       <CardActionArea onClick={handleCardClick}>
         <CardContent>
           <Grid
@@ -36,31 +36,32 @@ const PatientDetailCard = ({
               item
               xs={6}
               md={3}
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                flexDirection: "column",
-              }}
+              className={Styles.fcc}
+              // sx={{
+              //   display: "flex",
+              //   justifyContent: "center",
+              //   flexDirection: "column",
+              // }}
             >
-              <Typography variant="body1" gutterBottom sx={{ opacity: "60%" }}>
+              <Typography variant="body1" gutterBottom sx={{ opacity: '60%' }}>
                 Order No
               </Typography>
-              <Typography variant="body1" gutterBottom sx={{ opacity: "60%" }}>
+              <Typography variant="body1" gutterBottom sx={{ opacity: '60%' }}>
                 Patient Name
               </Typography>
-              <Typography variant="body1" gutterBottom sx={{ opacity: "60%" }}>
+              <Typography variant="body1" gutterBottom sx={{ opacity: '60%' }}>
                 UHID No
               </Typography>
-              <Typography variant="body1" gutterBottom sx={{ opacity: "60%" }}>
+              <Typography variant="body1" gutterBottom sx={{ opacity: '60%' }}>
                 Age (Y/M/D)
               </Typography>
-              <Typography variant="body1" gutterBottom sx={{ opacity: "60%" }}>
+              <Typography variant="body1" gutterBottom sx={{ opacity: '60%' }}>
                 Sex
               </Typography>
-              <Typography variant="body1" gutterBottom sx={{ opacity: "60%" }}>
+              <Typography variant="body1" gutterBottom sx={{ opacity: '60%' }}>
                 Status
               </Typography>
-              <Typography variant="body1" gutterBottom sx={{ opacity: "60%" }}>
+              <Typography variant="body1" gutterBottom sx={{ opacity: '60%' }}>
                 Doctor
               </Typography>
             </Grid>
@@ -68,58 +69,66 @@ const PatientDetailCard = ({
               item
               xs={6}
               md={3}
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                flexDirection: "column",
-              }}
+              className={Styles.fcc}
+              // sx={{
+              //   display: 'flex',
+              //   justifyContent: 'center',
+              //   flexDirection: 'column',
+              // }}
             >
               <Typography
                 variant="body1"
                 gutterBottom
-                sx={{ fontWeight: "bold" }}
+                className={Styles.bold}
+                // sx={{ fontWeight: 'bold' }}
               >
                 {orderNo}
               </Typography>
               <Typography
                 variant="body1"
                 gutterBottom
-                sx={{ fontWeight: "bold" }}
+                className={Styles.bold}
+                // sx={{ fontWeight: 'bold' }}
               >
                 {name}
               </Typography>
               <Typography
                 variant="body1"
                 gutterBottom
-                sx={{ fontWeight: "bold" }}
+                className={Styles.bold}
+                // sx={{ fontWeight: 'bold' }}
               >
                 {uhid}
               </Typography>
               <Typography
                 variant="body1"
                 gutterBottom
-                sx={{ fontWeight: "bold" }}
+                className={Styles.bold}
+                // sx={{ fontWeight: 'bold' }}
               >
                 {age}
               </Typography>
               <Typography
                 variant="body1"
                 gutterBottom
-                sx={{ fontWeight: "bold" }}
+                className={Styles.bold}
+                // sx={{ fontWeight: 'bold' }}
               >
                 {sex}
               </Typography>
               <Typography
                 variant="body1"
                 gutterBottom
-                sx={{ fontWeight: "bold" }}
+                className={Styles.bold}
+                // sx={{ fontWeight: 'bold' }}
               >
                 {status}
               </Typography>
               <Typography
                 variant="body1"
                 gutterBottom
-                sx={{ fontWeight: "bold" }}
+                className={Styles.bold}
+                // sx={{ fontWeight: 'bold' }}
               >
                 {doctor}
               </Typography>
