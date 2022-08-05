58cca6c52d8e689ebb0568aa27ab1904f9c90d1a
S Ajay <ajay.satishpriya@gmail.com>
2022-08-05 19:24:33 +0530

    💄 PDC ui-global

--- client/src/components/PatientDetailCard/PatientDetailCard.jsx
+++ client/src/components/PatientDetailCard/PatientDetailCard.jsx
@@ -5,23 +5,23 @@ import {
   Grid,
   IconButton,
   Typography,
-} from "@mui/material";
-import React from "react";
-import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import { useNavigate } from "react-router-dom";
-
+} from '@mui/material';
+import React from 'react';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { useNavigate } from 'react-router-dom';
+import Styles from './PatientDetailCard.module.css';
 const PatientDetailCard = ({ name, icon, link }) => {
   const navigate = useNavigate();
 
   return (
     <Card
       elevation={5}
-      sx={{ width: "100%", borderRadius: "10px", marginTop: "15px" }}
+      sx={{ width: '100%', borderRadius: '10px', marginTop: '15px' }}
     >
       <CardActionArea
         onClick={() => navigate(`/doctor/patient-details/${link}`)}
       >
-        <CardContent sx={{ display: "flex", padding: "4px 4px" }}>
+        <CardContent sx={{ display: 'flex', padding: '4px 4px' }}>
           <Grid
             container
             direction="row"
@@ -32,12 +32,13 @@ const PatientDetailCard = ({ name, icon, link }) => {
               item
               xs={3}
               md={3}
-              sx={{
-                display: "flex",
-                justifyContent: "flex-start",
-                alignItems: "center",
-                flexDirection: "column",
-              }}
+              className={Styles.grid1}
+              // sx={{
+              //   display: "flex",
+              //   justifyContent: "flex-start",
+              //   alignItems: "center",
+              //   flexDirection: "column",
+              // }}
             >
               <IconButton color="inherit">{icon}</IconButton>
             </Grid>
@@ -45,11 +46,12 @@ const PatientDetailCard = ({ name, icon, link }) => {
               item
               xs={7}
               md={3}
-              sx={{
-                display: "flex",
-                justifyContent: "flex-start",
-                flexDirection: "column",
-              }}
+              className={Styles.grid2}
+              // sx={{
+              //   display: 'flex',
+              //   justifyContent: 'flex-start',
+              //   flexDirection: 'column',
+              // }}
             >
               <Typography
                 variant="body1"
@@ -63,14 +65,15 @@ const PatientDetailCard = ({ name, icon, link }) => {
               item
               xs={2}
               md={3}
-              sx={{
-                display: "flex",
-                justifyContent: "flex-end",
-                flexDirection: "column",
-              }}
+              className={Styles.grid3}
+              // sx={{
+              //   display: 'flex',
+              //   justifyContent: 'flex-end',
+              //   flexDirection: 'column',
+              // }}
             >
               <IconButton color="inherit">
-                <KeyboardArrowRightIcon sx={{ opacity: "60%" }} />
+                <KeyboardArrowRightIcon sx={{ opacity: '60%' }} />
               </IconButton>
             </Grid>
           </Grid>
