b1c528522c9d02c0b867dedee9c89b9b85f1919c
S Ajay <ajay.satishpriya@gmail.com>
2022-08-05 18:24:39 +0530

    💄 header ui-global

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,26 +1,30 @@
-import React from "react";
-
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+import React from 'react';
+import Styles from './Header.module.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 const Header = ({ title }) => {
-	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static">
-				<Toolbar>
-					<Typography
-						variant="h6"
-						component="div"
-						sx={{ flexGrow: 1 }}
-					>
-						{title}
-					</Typography>
-				</Toolbar>
-			</AppBar>
-		</Box>
-	);
+  return (
+    <Box
+      className={Styles.flexGrow}
+      //sx={{ flexGrow: 1 }}
+    >
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            className={Styles.flexGrow}
+            // sx={{ flexGrow: 1 }}
+          >
+            {title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Header;
