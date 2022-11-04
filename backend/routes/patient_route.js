3e308a67007e8a021259bd6e77f06de95d03b333
os30722 <os30722@gmail.com>
2022-11-05 01:58:17 +0530

    added

--- backend/routes/patient_route.js
+++ backend/routes/patient_route.js
@@ -6,5 +6,6 @@ const action = require('../services/patient/action');
 router.use(require('../middlewares/authenticate'));
 
 router.post('/register', action.registerPatient);
+router.get('/vitals', action.findPatientVitals);
 
 module.exports = router;
